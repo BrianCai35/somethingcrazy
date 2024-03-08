bf8e074e0bc43fed53b196fa365ddf06c927e076
Brian <briancai35@gmail.com>
2024-03-08 11:24:11 -0500

    Fixed css not importing, added simple api grab script

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 ```sh
 npm create astro@latest -- --template minimal
 ```
+template used ^^^
 
 [Challenge link](https://devchallenges.io/editor/solution/14691)
+
+[netlift link](https://chipper-crepe-ed2dc7.netlify.app)
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,6 @@
-console.write("hello world");
+async function GetUser(username){
+    return await (await fetch("https://api.github.com/users/"+username)).json();
+}
+
+var user = GetUser("BrianCai35"); //async function always returns promise need fix later
+console.log(user);
