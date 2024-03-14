c3dfa6a9347354a15e2e53e7ab73d53bb9441a06
Brian <briancai35@gmail.com>
2024-03-14 01:26:47 -0400

    allows for the display of github api data

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,6 +1,20 @@
-async function GetUser(username){
-    return await (await fetch("https://api.github.com/users/"+username)).json();
+
+const userCard = document.getElementById("user-card-template");
+const cardContainer = document.getElementById("card-container");
+
+async function GetUserData(username){
+    const user = await (await fetch("https://api.github.com/users/"+username)).json();
+    return user; // returns promise
 }
 
-var user = GetUser("BrianCai35"); //async function always returns promise need fix later
-console.log(user);
+GetUserData("BrianCai35").then(user => {
+    const card = userCard.content.cloneNode(true).children[0];
+    const header = card.querySelector("[data-header]");
+    const body = card.querySelector("[data-body]");
+    header.textContent = user.login;
+    body.textContent = user.bio;
+    console.log(card);
+    cardContainer.append(card);
+}).catch(error => {
+    console.log("ERROR: "+ error);
+})
