74dae41329bea8ef78c5c4038472c6256e97f0d7
Brian <briancai35@gmail.com>
2024-03-19 11:10:05 -0400

    feat: allowed for displaying of information taken from github api

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,9 +1,9 @@
 
 const userCard = document.getElementById("user-card-template");
 const cardContainer = document.getElementById("card-container");
-const input = document.getElementsByClassName("search");
+const input = document.getElementById("search");
 
-input.addEventListener("input", e=>{
+input.addEventListener("input", e =>{
     const value = e.target.value;
     console.log(value);
 })
@@ -16,8 +16,10 @@ GetUserData("BrianCai35").then(user => {
     const card = userCard.content.cloneNode(true).children[0];
     const header = card.querySelector("[data-header]");
     const body = card.querySelector("[data-body]");
+    const icon = card.querySelector("[data-icon]");
     header.textContent = user.login;
     body.textContent = user.bio;
+    icon.src = user.avatar_url;
     console.log(card);
     cardContainer.append(card);
 }).catch(error => {
