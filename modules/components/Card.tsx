d2493e18f77a54e4bfd23831e8171f19f6a45b1d
Brian <briancai35@gmail.com>
2024-05-09 22:09:45 -0400

    Feat:finished Cards and animation

--- modules/components/Card.tsx
+++ modules/components/Card.tsx
@@ -18,10 +18,10 @@ export default function Card(props: PropsWithChildren<CardProps>): JSX.Element{
     return(
     <article className={`group relative rounded-xl bg-${color} p-5 content-end ${style} lg:w-72 lg:h-96 lg:pt-64 lg:hover:pt-0 lg:hover:pb-6 lg:ease-in-out lg:duration-[1500ms] overflow-hidden`}>
         <Paragraph theme="light" styles="lg:text-lg"><b>{`${number}`}</b></Paragraph>
-        <FaceLeftIcon color="black" styles="float-right md:absolute top-0 lg:right-3 " size="40px"/>
+        <FaceLeftIcon color="black" styles="float-right md:absolute top-2 md:-right-8" size="100"/>
         <h1 className={`${strasua} w-14 text-ivory text-3xl`} >{`${title}`}</h1>
         <hr className="ease-in-out lg:duration-1000 w-32 sm:w-72 lg:w-4 lg:group-hover:w-64 h-0.5 my-2 bg-gray-200 border-10 dark:bg-gray-700"></hr>
-        <Paragraph theme="light" weight="medium" styles="lg:text-base xl:text-sm ease-in-out delay-300 lg:duration-[500ms] lg:pt-10 lg:pb-5 lg:group-hover:pt-0 lg:leading-tight xl:leading-tight">{children}</Paragraph>
+        <Paragraph theme="light" weight="medium" styles="lg:text-base xl:text-base ease-in-out delay-[300ms] lg:duration-[500ms] lg:pt-10 lg:pb-2 lg:group-hover:pt-0 lg:leading-tight xl:leading-tight">{children}</Paragraph>
     </article>
 
 )}
